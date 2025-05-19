a787810066606b7f18e6a38519f28fe42e10fb52
flypiggy <150204110+flypiggyyoyoyo@users.noreply.github.com>
2025-05-19 22:53:59 +0800

    seata-grpc

--- MessagingService/src/main/java/com/shanyangcode/infinitechat/messagingservice/model/dto/ReceiveRedPacketRequest.java
+++
@@ -1,9 +0,0 @@
-package com.shanyangcode.infinitechat.messagingservice.model.dto;
-
-import lombok.Data;
-
-@Data
-public class ReceiveRedPacketRequest {
-    private Long userId;
-    private Long redPacketId;
-}
--- MessagingService/src/main/java/com/shanyangcode/infinitechat/messagingservice/model/entity/TextMessageBody.java
+++
@@ -1,9 +0,0 @@
-package com.shanyangcode.infinitechat.messagingservice.model.entity;
-
-import lombok.Data;
-
-@Data
-public class TextMessageBody {
-    private String content;
-    private Long replyId;
-}
